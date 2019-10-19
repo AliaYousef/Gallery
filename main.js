40c6e11823f1e594c8e84b399a60cd211a926a04
AliaYousef <55972055+AliaYousef@users.noreply.github.com>
2019-10-19 22:40:46 +0300

    Add files via upload
    new add

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 var images = document.getElementsByClassName("img");
+var name = "";
 for (var i = 0; i < images.length; i++) {
     images[i].addEventListener('click', onImageClick);
 
 }
 
 function onImageClick(e) {
+    document.getElementById("show").style.visibility = "visible"
     const index = event.target.getAttribute('src');
-    document.getElementById("add").innerHTML = '<div class = "newAdd added " ><img src = "'+ index + '" class = "img3"></div> <a href="#" id = "close" class ="close">X</a>';
-    console.log(index);
-    document.getElementById("close").addEventListener('click',function(){
-        document.getElementById("add").innerHTML = " ";
-    })
+    const Name = event.target.getAttribute('Name');
+    document.getElementById("add").innerHTML = '<img   src="' + index + '" class="img3"><span class="img3">'+ Name+' </span>';
+    num = event.target.getAttribute('value');
+    name = event.target.getAttribute('src');
+}
+
+document.getElementById("close").addEventListener('click', close);
+
+function close() {
+    document.getElementById("show").style.visibility = "hidden";
+}
+document.getElementById('first').addEventListener('click', pre);
+
+function pre(e) {
+    if (num== 0 ){
+        num = images.length;
+    document.getElementById("add").innerHTML = '<img   src="' + images[--num].src + '" class="img3">';
+    }else {
+        document.getElementById("add").innerHTML = '<img   src="' + images[--num].src + '" class="img3">'; 
+    }
+}
+document.getElementById('second').addEventListener('click', next);
+function next (){
+    if (num== images.length ){
+        num = 0;
+    document.getElementById("add").innerHTML = '<img   src="' + images[num++].src + '" class="img3">';
+    }else {
+        document.getElementById("add").innerHTML = '<img   src="' + images[num++].src + '" class="img3">'; 
+    }
 }
