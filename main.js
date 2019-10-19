5c39b6ab6d5ad0b2a8e25f9a8576a0e7d39537bd
AliaYousef <55972055+AliaYousef@users.noreply.github.com>
2019-10-19 23:31:47 +0300

    My version of gallery

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var images = document.getElementsByClassName("img");
+var titles = [ "The Lion King", "The Angry Birds 2","Car 3", "Lego Movie 2",  "Spider-Man", "Hotel Transylvania 3"]
 var name = "";
 for (var i = 0; i < images.length; i++) {
     images[i].addEventListener('click', onImageClick);
@@ -9,7 +10,7 @@ function onImageClick(e) {
     document.getElementById("show").style.visibility = "visible"
     const index = event.target.getAttribute('src');
     const Name = event.target.getAttribute('Name');
-    document.getElementById("add").innerHTML = '<img   src="' + index + '" class="img3"><span class="img3">'+ Name+' </span>';
+    document.getElementById("add").innerHTML = '<img   src="' + index + '" class="img3"><span class="text-block">'+Name+' </span>';
     num = event.target.getAttribute('value');
     name = event.target.getAttribute('src');
 }
@@ -24,17 +25,19 @@ document.getElementById('first').addEventListener('click', pre);
 function pre(e) {
     if (num== 0 ){
         num = images.length;
-    document.getElementById("add").innerHTML = '<img   src="' + images[--num].src + '" class="img3">';
+    document.getElementById("add").innerHTML = '<img   src="' + images[--num].src + '" class="img3"><span class="text-block">'+titles[num]+' </span>';
     }else {
-        document.getElementById("add").innerHTML = '<img   src="' + images[--num].src + '" class="img3">'; 
+        document.getElementById("add").innerHTML = '<img   src="' + images[--num].src + '" class="img3"><span class="text-block">'+titles[num]+' </span>'; 
     }
 }
 document.getElementById('second').addEventListener('click', next);
 function next (){
     if (num== images.length ){
-        num = 0;
-    document.getElementById("add").innerHTML = '<img   src="' + images[num++].src + '" class="img3">';
+        num = -1;
+        num++
+    document.getElementById("add").innerHTML = '<img   src="' + images[num].src + '" class="img3"><span class="text-block">'+titles[num]+' </span>';
     }else {
-        document.getElementById("add").innerHTML = '<img   src="' + images[num++].src + '" class="img3">'; 
+        document.getElementById("add").innerHTML = '<img   src="' + images[num].src + '" class="img3"><span class="text-block">'+titles[num]+' </span>';
+        num++
     }
 }
